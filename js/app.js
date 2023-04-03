692dd98c1d711cefa6a6eaf9d7f228fbb3e813a6
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-04-03 16:23:22 -0700

    initial js constants and states

--- js/app.js
+++ js/app.js
@@ -1,11 +1,55 @@
-console.log("JavaScript Linked");
+/*----- Setting up Game -----*/
+// 1. Player chooses where to place their ships on the board
+// 2. Ships have a specific set of hps
+// 3. Place the ship objects into each array idx player clicks
 
 /*----- constants -----*/
+const ships = {
+  carrier: {
+    hp: 5
+  },
+  battleship: {
+    hp: 4
+  },
+  cruiser: {
+    hp: 3
+  },
+  submarine: {
+    hp: 3
+  },
+  destroyer: {
+    hp: 2
+  }
+};
 
 /*----- state variables -----*/
+let turn;
+let winner;
+
+// (10 X 10) 2d array
+// values are 0 (empty), 1 (a ship is there), -1 (no ship is there)
+let playerBoard;
+let computerBoard;
 
 /*----- cached elements  -----*/
+const playAgainBtn = document.getElementById("playAgain");
+const computerBoardEls = document.querySelectorAll(
+  "#computer > .display > .board > div"
+);
+const playerBoardEls = document.querySelectorAll(
+  "#player > .display > .board > div"
+);
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+
+function init() {
+  turn = 1;
+  winner = null; // (1 or -1) no ties
+  computerBoard = Array.from(new Array(10), () => new Array(10).fill(0));
+  playerBoard = Array.from(new Array(10), () => new Array(10).fill(0));
+}
+
+/*----- initialize game -----*/
+init();
