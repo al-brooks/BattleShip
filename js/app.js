2553f011dc3202a42ce24529cada503445a4c1ca
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-04-03 16:52:47 -0700

    built out inital ship objects

--- js/app.js
+++ js/app.js
@@ -1,24 +1,72 @@
 /*----- Setting up Game -----*/
+// Player:
 // 1. Player chooses where to place their ships on the board
 // 2. Ships have a specific set of hps
 // 3. Place the ship objects into each array idx player clicks
+// 4. Ships are either vertical or horizontal
+
+// Computer:
+// 1. Computer's ships are randomly selected.
+// 2. They need to be within the board bounds, and no conflicting with other ships
+// 3. If you pick a point, you can check to see availability to the left, right
+// top and bottom, going for the amount of available spaces.
+// 4. The first available combination is chosen.
 
 /*----- constants -----*/
-const ships = {
+
+const playerShips = {
   carrier: {
-    hp: 5
+    hp: 5,
+    spaces: 5,
+    coordinates: []
   },
   battleship: {
-    hp: 4
+    hp: 4,
+    spaces: 4,
+    coordinates: []
   },
   cruiser: {
-    hp: 3
+    hp: 3,
+    spaces: 3,
+    coordinates: []
   },
   submarine: {
-    hp: 3
+    hp: 3,
+    spaces: 3,
+    coordinates: []
   },
   destroyer: {
-    hp: 2
+    hp: 2,
+    spaces: 2,
+    coordinates: []
+  }
+};
+
+const computerShips = {
+  carrier: {
+    hp: 5,
+    spaces: 5,
+    coordinates: []
+  },
+  battleship: {
+    hp: 4,
+    spaces: 4,
+    coordinates: []
+  },
+  cruiser: {
+    hp: 3,
+    spaces: 3,
+    coordinates: []
+  },
+  submarine: {
+    hp: 3,
+    spaces: 3,
+    coordinates: []
+  },
+  destroyer: {
+    hp: 2,
+    spaces: 2,
+    coordinates: []
   }
 };
 
