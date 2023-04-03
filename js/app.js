e0f24260a77b4740d8d0a6689b2aea2dfae91c66
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-04-03 13:35:19 -0700

    html computer board skeleton

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 console.log("JavaScript Linked");
+
+/*----- constants -----*/
+
+/*----- state variables -----*/
+
+/*----- cached elements  -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
