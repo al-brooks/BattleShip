58ba20d884fa033df3c06109bc13d1f7f49068ec
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-04-04 09:57:18 -0700

    added btns for adding ships

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 /*----- Setting up Game -----*/
+/*
 // Player:
-// 1. Player chooses where to place their ships on the board
-// 2. Ships have a specific set of hps
-// 3. Place the ship objects into each array idx player clicks
-// 4. Ships are either vertical or horizontal
+    1. Player chooses where to place their ships on the board
+        a. Player clicks on an 'add' btn that's next to the ship they want to add
+            i. JS knows that the player is adding a ship (toggle)
+        b. Player is prompted to click on grid squares they want their ship on
+            1. Program checks that the space is unoccupied.
+            2. Click updates the coordinates of ship
+            3. Decreases the number of available spaces on ship
+            4. playerBoard array idx [i][j] is updated from 0 to ship object
+        c. Once ship available spaces reaches 0
+            1. Player cannot click on any other squares.
+            2. Player is prompted to click complete or reset (if they don't like arrangement)
+        d. Once Player submits choice
+            1. Program verifies that the coordinates for the ship are in sequential order.
+            2. Are they in a straight line vertically or horizontally.
+            3. If placement is invalid, Player must redo selection.
+            4. playerBoard at ship's coordinates are reset to 0
+    2. Ships have a specific set of hp which is decremented whenever hit.
+
 
 // Computer:
 // 1. Computer's ships are randomly selected.
@@ -11,6 +26,8 @@
 // 3. If you pick a point, you can check to see availability to the left, right
 // top and bottom, going for the amount of available spaces.
 // 4. The first available combination is chosen.
+
+*/
 
 /*----- constants -----*/
 
@@ -95,8 +112,8 @@ const playerBoardEls = document.querySelectorAll(
 function init() {
   turn = 1;
   winner = null; // (1 or -1) no ties
-  computerBoard = Array.from(new Array(10), () => new Array(10).fill(0));
-  playerBoard = Array.from(new Array(10), () => new Array(10).fill(0));
+  computerBoard = Array.from(new Array(10), () => new Array(10).fill(0)); // 0 or Computer Ships
+  playerBoard = Array.from(new Array(10), () => new Array(10).fill(0)); // 0 or Player Ships
 }
 
 /*----- initialize game -----*/
